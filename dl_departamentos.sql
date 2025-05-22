3e3873ae4349203c621638ab4e695c3d8b3a8416
Saren-Cased <urdi.cased.5@gmail.com>
2025-05-21 22:07:00 -0500

    Add files via upload

---
+++ dl_departamentos.sql
@@ -0,0 +1,13 @@
+-- Data_Lake.dbo.dl_departamentos definition
+
+-- Drop table
+
+-- DROP TABLE Data_Lake.dbo.dl_departamentos;
+
+CREATE TABLE Data_Lake.dbo.dl_departamentos (
+	COD_DPTO tinyint NULL,
+	NOM_DPTO nvarchar(60) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	LATITUD float NULL,
+	LONGITUD float NULL,
+	[Geo Departamento] varchar(50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+);
---
+++ dl_municipios.sql
@@ -0,0 +1,16 @@
+-- Data_Lake.dbo.dl_municipios definition
+
+-- Drop table
+
+-- DROP TABLE Data_Lake.dbo.dl_municipios;
+
+CREATE TABLE Data_Lake.dbo.dl_municipios (
+	COD_DPTO tinyint NULL,
+	NOM_DPTO nvarchar(60) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	COD_MPIO int NULL,
+	NOM_MPIO varchar(30) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	TIPO varchar(25) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	LATITUD float NULL,
+	LONGITUD float NULL,
+	[Geo Municipio] varchar(50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+);
